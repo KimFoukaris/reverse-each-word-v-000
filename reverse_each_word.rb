88b7d0591f22baa05d6ec346059a0782e94a8526
Kim Foukaris <kimfoukaris@outlook.com>
2018-01-27 01:19:07 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 def reverse_each_word(sentence)
   words = sentence.split
-  words_reversed = []
-  words.each {|word| words_reversed << word.reverse}
-  words_reversed.join(" ")
+  #words_reversed = []
+  #words.each {|word| words_reversed << word.reverse}
+  #words_reversed.join(" ")
+  words.collect {|word| word.reverse}
 end
